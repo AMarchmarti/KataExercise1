b6a54487a59a5cdd34f4667d4ff792ab54268986
Antoni March Marti <amarch@brujula.es>
2019-04-11 12:44:59 +0200

    Se da por finalizado el ejercicio, pasa los casos test

--- src/main/java/org/brujula/Kata.java
+++ src/main/java/org/brujula/Kata.java
@@ -4,12 +4,12 @@ package org.brujula;
 public class Kata{
     public static String declareWinner(Fighter luchador, Fighter contrincante){
         while ((luchador.getHealth() > 0) || (contrincante.getHealth() > 0)){
-            luchador.combat(contrincante.getAttack());
             contrincante.combat(luchador.getAttack());
-        }
-        if (luchador.getHealth() < contrincante.getHealth()){
-            return contrincante.getName();
-        }
+            if (luchador.getHealth() <= 0){
+                return contrincante.getName();
+            }else{
+            luchador.combat(contrincante.getAttack());
+        }}
         return luchador.getName();
     }
 }
