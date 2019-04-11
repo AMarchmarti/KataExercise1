916d48e6e373b85abe6efad638a7d52627c379b1
Antoni March Marti <amarch@brujula.es>
2019-04-11 12:44:25 +0200

    Modificado los casos test

--- src/test/java/org/brujula/FighterTest.java
+++ src/test/java/org/brujula/FighterTest.java
@@ -20,8 +20,8 @@ public class FighterTest {
 
     @Test
     public void basicTests() {
-        //assertEquals("Lew", Kata.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4)));
-        assertEquals("Harry", Kata.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4)));
+        assertEquals("Lew", Kata.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4)));
+        assertEquals("Lew", Kata.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4)));
         assertEquals("Harald", Kata.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4)));
         assertEquals("Harald", Kata.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4)));
         assertEquals("Harald", Kata.declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5)));
