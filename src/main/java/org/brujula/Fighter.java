383a3a0dfb5b990bfce1b5e32dd4227c5f4db43a
Antoni March Marti <amarch@brujula.es>
2019-04-11 12:10:33 +0200

    Se construirá la clase fighter y pasará los casos test

---
+++ src/main/java/org/brujula/Fighter.java
@@ -0,0 +1,36 @@
+package org.brujula;
+
+public class Fighter {
+
+    //Atributos
+    private String name = null;
+    private Integer health = 0;
+    private Integer attack = 0;
+
+    //Constructor
+    public Fighter(String name, Integer health, Integer attack){
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+    }
+
+    //Setters
+
+    public void setHealth(Integer health) {
+        this.health -= health;
+    }
+
+    //Getters
+    public Integer getAttack() {
+        return attack;
+    }
+
+    public Integer getHealth() {
+        return health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
