208c4b42b3ff5946e3fa86431ddf7b030b764fbb
Antoni March Marti <amarch@brujula.es>
2019-04-11 12:04:36 +0200

    Se añadirán casos test para la clase Fighter

--- src/test/java/org/brujula/FighterTest.java
+++ src/test/java/org/brujula/FighterTest.java
@@ -7,6 +7,17 @@ import org.junit.Test;
 
 
 public class FighterTest {
+
+    @Test
+    public void fighterTest(){
+        Fighter fighter = new Fighter("Toni", 10, 10);
+        setLife(5);
+        assertEquals(5, fighter.getLife());
+        assertEquals(10, fighter.getAttack);
+        assertEquals("Toni", fighter.getName());
+        //assertEquals(0, fighter.combat());
+    }
+    /*
     @Test
     public void basicTests() {
         assertEquals("Lew", Kata.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4)));
@@ -15,5 +26,5 @@ public class FighterTest {
         assertEquals("Harald", Kata.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4)));
         assertEquals("Harald", Kata.declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5)));
         assertEquals("Harald", Kata.declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5)));
-    }
+    }*/
 }
