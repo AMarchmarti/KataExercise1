b34fd62a3c0e9b5773b5369c44b06922b5ee0e00
Antoni March Marti <amarch@brujula.es>
2019-04-12 08:25:29 +0200

    Se solucionan los problemas de quien golpea primero

--- src/main/java/org/brujula/Kata.java
+++ src/main/java/org/brujula/Kata.java
@@ -1,15 +1,34 @@
 package org.brujula;
 
 
-public class Kata{
-    public static String declareWinner(Fighter luchador, Fighter contrincante){
-        while ((luchador.getHealth() > 0) || (contrincante.getHealth() > 0)){
+import javax.jnlp.FileContents;
+
+public class Kata {
+
+    public static Boolean esPrimero(Fighter luchador, String primerGolpe) {
+        if (primerGolpe.equals(luchador.getName())) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String combate(Fighter luchador, Fighter contrincante) {
+        while ((luchador.getHealth() > 0) || (contrincante.getHealth() > 0)) {
             contrincante.combat(luchador.getAttack());
-            if (luchador.getHealth() <= 0){
+            if (luchador.getHealth() <= 0) {
                 return contrincante.getName();
-            }else{
-            luchador.combat(contrincante.getAttack());
-        }}
+            } else {
+                luchador.combat(contrincante.getAttack());
+            }
+        }
         return luchador.getName();
+    }
+
+
+    public static String declareWinner(Fighter luchador, Fighter contrincante, String primerGolpe) {
+        if (esPrimero(luchador, primerGolpe)) {
+            return combate(luchador, contrincante);
+        }
+         return combate(contrincante, luchador);
     }
 }
